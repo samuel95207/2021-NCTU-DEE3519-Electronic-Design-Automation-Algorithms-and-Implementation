9c3f4064a1850775dfad89eecf02ca7c67d14b4c
Samuel Huang <lspss95207@gmail.com>
2021-05-14 14:03:44 +0800

    Split src files

---
+++ Lab3/src/BDDInterface.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unistd.h>
+
+#include "util.h"
+#include "cudd.h"
+
+using namespace std;
+
+
+class BDDInterfacec
+{
+    DdManager *gbm;
+    DdNode *bdd;
+    unordered_map<char, DdNode *> nodeMap;
+    unordered_map<char, double> probabilityMap;
+    unordered_map<int, char> inputOrderMap;
+
+    double probability;
+
+public:
+    BDDInterfacec();
+    ~BDDInterfacec();
+
+    void setBooleanFunction(string &str);
+    void setProbability(char c, double probability);
+
+    double calculateProbability(string tmpFilename);
+
+    void printBDD(int n, int pr);
+    void printProbabilityMap();
+    void printVariables();
+
+    void writeGraphFile(char *filename);
+
+    friend std::istream &operator>>(std::istream &in, BDDInterfacec &BDD);
+    friend std::ostream &operator<<(std::ostream &out, const BDDInterfacec &BDD);
+};
---
+++ Lab3/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "util.h"
+#include "cudd.h"
+
+#include "BDDInterface.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    BDDInterfacec BDD;
+    ifstream infile(argv[1]);
+    infile >> BDD;
+    infile.close();
+
+    // BDD.printProbabilityMap();
+    // BDD.printBDD(2, 4);
+    // BDD.printVariables();
+    // BDD.writeGraphFile("graph.dot");
+
+    BDD.calculateProbability(argv[2]);
+
+
+    ofstream outfile(argv[2]);
+    outfile<<BDD;
+
+}
